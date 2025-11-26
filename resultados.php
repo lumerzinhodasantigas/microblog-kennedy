7db059092ca4049f05b5d3a0be8f3289ffbc24a5
Kenny <shadow.dio.shadow@gmail.com>
2025-11-26 16:20:23 -0300

    Update resultados.php

--- resultados.php
+++ resultados.php
@@ -11,7 +11,6 @@ $termo = Utils::sanitizar($_GET['busca']);
 
 try {
     $dados = $noticiaServico->buscarNoticias($termo);
-    Utils::dump($dados);
 } catch (Throwable $e) {
     $erro = "Erro ao fazer a busca no sistema. <br>".$e->getMessage();
 }
@@ -23,29 +22,43 @@ require_once "includes/cabecalho.php";
 <?php endif; ?>
  
 <div class="row my-1 mx-md-n1">
+
     <h2 class="col-12 fs-5 fw-light">
         Você procurou por 
-        <span class="badge bg-dark"> termo digitado... </span> e
-        obteve <span class="badge bg-info">  X </span> resultados
+        <span class="badge bg-dark">  <?= $termo ?> </span> e
+        obteve <span class="badge bg-info"> <?= count($dados) ?> </span> resultados
     </h2>
-    
 
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">
-                    Título da notícia...
-                </h3>
-                <p class="card-text">
-                    <time>11/11/2011 21:12</time> - 
-                    Resumo da notícia...
-                </p>
-                
-                <a href="noticia.php" 
-                class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
+    <!-- Se o resultado da busca for zero, faça aparecer: -->
+
+    <?php if (empty($dados)): ?>
+        <p class="alert alert-warning text-center"> Nenhum resultado encontrado! </p>
+    <?php else: ?>
+
+    <!-- Caso contrário, faça aparecer a div abaixo (usando o foreach) -->
+     
+    <?php foreach ($dados as $noticia): ?>
+
+        <div class="col-12 my-1">
+            <article class="card">
+                <div class="card-body">
+                    <h3 class="fs-4 card-title fw-light">
+                        <?= $noticia['titulo'] ?>
+                    </h3>
+                    <p class="card-text">
+                        <time datetime="<?= $noticia['data'] ?>"> 
+                            <?= Utils::formatarData($noticia['data']) ?>
+                        </time> - 
+                        <?= $noticia['resumo'] ?>
+                    </p>
+                    
+                    <a href="noticia.php?id=<?= $noticia['id'] ?>" 
+                    class="btn btn-primary btn-sm">Continuar lendo</a>
+                </div>
+            </article>
+        </div>
+        <?php endforeach; ?>
+        <?php endif; ?>
 
 </div>     
 
